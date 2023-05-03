e448bc74bcb01e141437d5ff9ee2b3b5c9097ec0
max whitehead <fifes-catkin.0r@icloud.com>
2023-05-03 13:48:38 +0200

    Added New Investments page - Testing use of gradients on grid

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,29 +1,29 @@
-const Dashboard = () => {
+const Overview = () => {
   return (
     <>
       <div className="p-4 sm:ml-64">
         <div className="p-4 mt-14">
             <div>
-              <h1 className="text-4xl font-semibold text-gray-800 dark:text-gray-100 my-8">Dashboard</h1>
+              <h1 className="text-4xl font-semibold text-gray-800 dark:text-gray-100 my-8">Overview</h1>
             </div>
-            <div className="flex items-center justify-center h-60 mb-4 rounded bg-gray-50 dark:bg-gray-800">
+            <div className="flex items-center justify-center h-60 mb-4 rounded-xl bg-gray-50 dark:bg-gray-800">
               <p className="text-2xl text-gray-400 dark:text-gray-500">+</p>
             </div>
             <div className="grid grid-cols-2 gap-4 mb-4">
-              <div className="flex items-center justify-center rounded bg-gray-50 h-48 dark:bg-gray-800">
+              <div className="flex items-center justify-center rounded-xl bg-gray-50 h-48 dark:bg-gray-800">
                   <p className="text-2xl text-gray-400 dark:text-gray-500">+</p>
               </div>
-              <div className="flex items-center justify-center rounded bg-gray-50 h-48 dark:bg-gray-800">
+              <div className="flex items-center justify-center rounded-xl bg-gray-50 h-48 dark:bg-gray-800">
                   <p className="text-2xl text-gray-400 dark:text-gray-500">+</p>
               </div>
-              <div className="flex items-center justify-center rounded bg-gray-50 h-48 dark:bg-gray-800">
+              <div className="flex items-center justify-center rounded-xl bg-gray-50 h-48 dark:bg-gray-800">
                   <p className="text-2xl text-gray-400 dark:text-gray-500">+</p>
               </div>
-              <div className="flex items-center justify-center rounded bg-gray-50 h-48 dark:bg-gray-800">
+              <div className="flex items-center justify-center rounded-xl bg-gray-50 h-48 dark:bg-gray-800">
                   <p className="text-2xl text-gray-400 dark:text-gray-500">+</p>
               </div>
             </div>
-            <div className="flex items-center justify-center h-60 mb-4 rounded bg-gray-50 dark:bg-gray-800">
+            <div className="flex items-center justify-center h-60 mb-4 rounded-xl bg-gray-50 dark:bg-gray-800">
               <p className="text-2xl text-gray-400 dark:text-gray-500">+</p>
             </div>
           </div>
@@ -32,4 +32,4 @@ const Dashboard = () => {
   );
 }
 
-export default Dashboard;
+export default Overview;
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,13 @@ export default {
   content: ['./src/**/*.{js,jsx}', 'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'],
   darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      "rounded": {
+        "xl": "1rem",
+        "2xl": "1.5rem",
+        "3xl": "2rem",
+      }
+    },
   },
   plugins: [
     require('flowbite/plugin')
