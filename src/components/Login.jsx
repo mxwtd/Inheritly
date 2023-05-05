96a9125fdad0819585c300a76635f26b7e194002
max whitehead <fifes-catkin.0r@icloud.com>
2023-05-05 10:20:57 +0100

    Catch up main

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -22,15 +22,15 @@ const Login = () => {
                           <input type="password" name="password" id="password" placeholder="••••••••" className="bg-slate-50/[.3] border border-slate-500 text-slate-700 sm:text-sm rounded-lg focus:ring-slate-600 focus:border-slate-600 block w-full p-2.5 dark:bg-slate-700/[.3] dark:border-slate-600 dark:placeholder-slate-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required=""/>
                       </div>
                       <div className="flex items-center justify-between">
-                          <div className="flex items-start">
-                            <div className="flex items-center h-5">
-                              <input id="remember" aria-describedby="remember" type="checkbox" className="w-4 h-4 border border-slate-300 rounded bg-slate-50 focus:ring-3 focus:ring-slate-300 dark:bg-slate-700 dark:border-slate-600 dark:focus:ring-slate-600 dark:ring-offset-slate-800" required=""/>
-                            </div>
-                            <div className="ml-3 text-sm">
-                              <label htmlFor="remember" className="text-slate-600 dark:text-slate-300">Remember me</label>
-                            </div>
+                        <div className="flex items-start">
+                          <div className="flex items-center h-5">
+                            <input id="remember" aria-describedby="remember" type="checkbox" className="w-4 h-4 border border-slate-300 rounded bg-slate-50 focus:ring-3 focus:ring-slate-300 dark:bg-slate-700 dark:border-slate-600 dark:focus:ring-slate-600 dark:ring-offset-slate-800" required=""/>
                           </div>
-                          <a href="#" className="text-sm font-medium text-slate-600 hover:underline dark:text-slate-400">Forgot password?</a>
+                          <div className="ml-3 text-sm">
+                            <label htmlFor="remember" className="text-slate-600 dark:text-slate-300">Remember me</label>
+                          </div>
+                        </div>
+                        <a href="#" className="text-sm font-medium text-slate-600 hover:underline dark:text-slate-400">Forgot password?</a>
                       </div>
                       <button type="submit" className="w-full text-slate-600 hover:text-slate-300 bg-slate-400/[.3] hover:bg-slate-600 focus:ring-4 focus:outline-none focus:ring-slate-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-slate-500 dark:hover:bg-slate-700 dark:focus:ring-slate-800 dark:text-slate-300 dark:hover:text-slate-100">Sign in</button>
                       <p className="text-sm font-light text-slate-500 dark:text-slate-400">
