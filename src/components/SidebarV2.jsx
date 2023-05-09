d1de52c8dc7fefe68bd70a8fa92bb2728cb28f8e
max whitehead <fifes-catkin.0r@icloud.com>
2023-05-09 16:05:29 +0100

    Modified responsiveness 2

--- src/components/SidebarV2.jsx
+++ src/components/SidebarV2.jsx
@@ -11,7 +11,7 @@ const SidebarV2 = () => {
                   <path clipRule="evenodd" fillRule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
                 </svg>
               </button>
-              <a href="https://flowbite.com" className="flex ml-2 md:mr-24">
+              <a className="flex ml-2 md:mr-24">
                 <span className="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap dark:text-white">Inheritly</span>
               </a>
             </div>
