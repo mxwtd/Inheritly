bcb379052f6350feba69638047dc2834a301815a
Santiago Ospina Idrobo <santiagospidrobo15@gmail.com>
2023-05-10 18:35:38 +0200

    add react dom and create components to the sidebar

---
+++ app/src/components/Manage.jsx
@@ -0,0 +1,15 @@
+const Manage = () => {
+  return (
+    <>
+      <div className='p-4 sm:ml-64'>
+        <div className='p-4 mt-14'>
+          <div>
+            <h1 className='text-4xl font-semibold text-gray-800 dark:text-gray-100 my-8'>Manage</h1>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Manage
