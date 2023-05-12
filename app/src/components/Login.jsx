e12078fcf5bae0ebbb534902ce683ea88d665bdd
max whitehead <fifes-catkin.0r@icloud.com>
2023-05-12 22:46:08 +0100

    Added Password reset

--- app/src/components/Login.jsx
+++ app/src/components/Login.jsx
@@ -2,10 +2,16 @@ import { useState } from 'react'
 
 const Login = () => {
   const [isSignUp, setIsSignUp] = useState(false)
+  const [isForgotPassword, setIsForgotPassword] = useState(false)
 
   const toggleSignUp = (e) => {
     e.preventDefault()
     setIsSignUp(!isSignUp)
+  }
+
+  const toggleForgotPassword = (e) => {
+    e.preventDefault()
+    setIsForgotPassword(!isForgotPassword) // toggle isForgotPassword state
   }
   return (
     <>
@@ -17,10 +23,10 @@ const Login = () => {
           <div className='w-full bg-white/[.09] backdrop-blur-md rounded-xl shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-slate-800/[.3] dark:border-slate-700'>
             <div className='p-6 space-y-4 md:space-y-6 sm:p-8'>
               <h1 className='text-xl font-bold leading-tight tracking-tight text-slate-700 md:text-2xl dark:text-white'>
-                {isSignUp ? 'Create an account' : 'Sign in to your account'}
+                {isSignUp ? 'Create an account' : isForgotPassword ? 'Reset Password' : 'Sign in to your account'}
               </h1>
               <form className='space-y-4 md:space-y-6' action='#'>
-                {isSignUp && (
+                {!isForgotPassword && isSignUp && (
                   <div>
                     <label htmlFor='name' className='block mb-2 text-sm font-medium text-slate-700 dark:text-white'>Your name</label>
                     <input type='text' name='name' id='name' className='bg-slate-50/[.3] border border-slate-500 text-slate-700 sm:text-sm rounded-lg focus:ring-slate-600 focus:border-slate-600 block w-full p-2.5 dark:bg-slate-700/[.3] dark:border-slate-600 dark:placeholder-slate-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500' placeholder='Your name' required='' />
@@ -30,31 +36,53 @@ const Login = () => {
                   <label htmlFor='email' className='block mb-2 text-sm font-medium text-slate-700 dark:text-white'>Your email</label>
                   <input type='email' name='email' id='email' className='bg-slate-50/[.3] border border-slate-500 text-slate-700 sm:text-sm rounded-lg focus:ring-slate-600 focus:border-slate-600 block w-full p-2.5 dark:bg-slate-700/[.3] dark:border-slate-600 dark:placeholder-slate-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500' placeholder='name@company.com' required='' />
                 </div>
-                <div>
-                  <label htmlFor='password' className='block mb-2 text-sm font-medium text-slate-700 dark:text-white'>Password</label>
-                  <input type='password' name='password' id='password' placeholder='••••••••' className='bg-slate-50/[.3] border border-slate-500 text-slate-700 sm:text-sm rounded-lg focus:ring-slate-600 focus:border-slate-600 block w-full p-2.5 dark:bg-slate-700/[.3] dark:border-slate-600 dark:placeholder-slate-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500' required='' />
-                </div>
-                {isSignUp && (
-                  <div>
-                    <label htmlFor='repeatPassword' className='block mb-2 text-sm font-medium text-slate-700 dark:text-white'>Repeat password</label>
-                    <input type='password' name='repeatPassword' id='repeatPassword' placeholder='••••••••' className='bg-slate-50/[.3] border border-slate-500 text-slate-700 sm:text-sm rounded-lg focus:ring-slate-600 focus:border-slate-600 block w-full p-2.5 dark:bg-slate-700/[.3] dark:border-slate-600 dark:placeholder-slate-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500' required='' />
-                  </div>
+                {!isForgotPassword && (
+                  <>
+                    <div>
+                      <label htmlFor='password' className='block mb-2 text-sm font-medium text-slate-700 dark:text-white'>Password</label>
+                      <input type='password' name='password' id='password' placeholder='••••••••' className='bg-slate-50/[.3] border border-slate-500 text-slate-700 sm:text-sm rounded-lg focus:ring-slate-600 focus:border-slate-600 block w-full p-2.5 dark:bg-slate-700/[.3] dark:border-slate-600 dark:placeholder-slate-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500' required='' />
+                    </div>
+                    {isSignUp && (
+                      <div>
+                        <label htmlFor='repeatPassword' className='block mb-2 text-sm font-medium text-slate-700 dark:text-white'>Repeat password</label>
+                        <input type='password' name='repeatPassword' id='repeatPassword' placeholder='••••••••' className='bg-slate-50/[.3] border border-slate-500 text-slate-700 sm:text-sm rounded-lg focus:ring-slate-600 focus:border-slate-600 block w-full p-2.5 dark:bg-slate-700/[.3] dark:border-slate-600 dark:placeholder-slate-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500' required='' />
+                      </div>
+                    )}
+                  </>
                 )}
-                <div className='flex items-center justify-between'>
-                  <div className='flex items-start'>
-                    <div className='flex items-center h-5'>
-                      <input id='remember' aria-describedby='remember' type='checkbox' className='w-4 h-4 border border-slate-300 rounded bg-slate-50 focus:ring-3 focus:ring-blue-300 dark:bg-slate-700 dark:border-slate-600 dark:focus:ring-slate-600 dark:ring-offset-slate-800' required='' />
+                {!isForgotPassword && (
+                  <>
+                    <div className='flex items-center justify-between'>
+                      <div className='flex items-start'>
+                        <div className='flex items-center h-5'>
+                          <input id='remember' aria-describedby='remember' type='checkbox' className='w-4 h-4 border border-slate-300 rounded bg-slate-50 focus:ring-3 focus:ring-blue-300 dark:bg-slate-700 dark:border-slate-600 dark:focus:ring-slate-600 dark:ring-offset-slate-800' required='' />
+                        </div>
+                        <div className='ml-3 text-sm'>
+                          <label htmlFor='remember' className='text-slate-600 dark:text-slate-300'>Remember me</label>
+                        </div>
+                      </div>
+                      {!isSignUp && !isForgotPassword && (
+                        <a href='#' onClick={toggleForgotPassword} className='text-sm font-medium text-slate-600 hover:underline dark:text-slate-400'>Forgot password?</a>
+                      )}
                     </div>
-                    <div className='ml-3 text-sm'>
-                      <label htmlFor='remember' className='text-slate-600 dark:text-slate-300'>Remember me</label>
+                    <button type='submit' className='w-full text-slate-600 hover:text-slate-300 bg-slate-400/[.3] hover:bg-slate-600 focus:ring-4 focus:outline-none focus:ring-slate-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-slate-500 dark:hover:bg-slate-700 dark:focus:ring-slate-800 dark:text-slate-300 dark:hover:text-slate-100'>{isSignUp ? 'Sign up' : isForgotPassword ? 'Reset password' : 'Sign in'}</button>
+                  </>
+                )}
+                {isForgotPassword && (
+                  <>
+                    <div>
+                      <button type='submit' className='w-full text-slate-600 hover:text-slate-300 bg-slate-400/[.3] hover:bg-slate-600 focus:ring-4 focus:outline-none focus:ring-slate-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-slate-500 dark:hover:bg-slate-700 dark:focus:ring-slate-800 dark:text-slate-300 dark:hover:text-slate-100'>Send reset link</button>
                     </div>
-                  </div>
-                  <a href='#' className='text-sm font-medium text-slate-600 hover:underline dark:text-slate-400'>Forgot password?</a>
-                </div>
-                <button type='submit' className='w-full text-slate-600 hover:text-slate-300 bg-slate-400/[.3] hover:bg-slate-600 focus:ring-4 focus:outline-none focus:ring-slate-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-slate-500 dark:hover:bg-slate-700 dark:focus:ring-slate-800 dark:text-slate-300 dark:hover:text-slate-100'>{isSignUp ? 'Sign up' : 'Sign in'}</button>
-                <p className='text-sm font-light text-slate-500 dark:text-slate-400'>
-                  <button onClick={toggleSignUp} className='p-2 rounded-lg font-medium text-slate-600 hover:underline dark:text-slate-500'>{isSignUp ? 'Already have an account?' : 'Sign up'}</button>
-                </p>
+                    <p className='text-sm font-light text-slate-500 dark:text-slate-400'>
+                      <button onClick={toggleForgotPassword} className='p-2 rounded-lg font-medium text-slate-600 hover:underline dark:text-slate-500'>Return to sign in</button>
+                    </p>
+                  </>
+                )}
+                {!isForgotPassword && (
+                  <p className='text-sm font-light text-slate-500 dark:text-slate-400'>
+                    <button onClick={toggleSignUp} className='p-2 rounded-lg font-medium text-slate-600 hover:underline dark:text-slate-500'>{isSignUp ? 'Already have an account?' : 'Sign up'}</button>
+                  </p>
+                )}
               </form>
             </div>
           </div>
