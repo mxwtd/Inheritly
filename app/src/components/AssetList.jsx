82bc40170f6720d55a377801c7fd5fa572b47cd2
max whitehead <fifes-catkin.0r@icloud.com>
2023-05-12 11:45:11 +0100

    Merged Investment form into Asset List

--- app/src/components/AssetList.jsx
+++ app/src/components/AssetList.jsx
@@ -1,10 +1,80 @@
+import { useState } from 'react'
+
 const AssetList = () => {
+  // Modal Open and Close
+  const [isModalOpen, setIsModalOpen] = useState(false)
+
   return (
     <>
       <div className='p-4 sm:ml-64 h-screen'>
         <div className='p-4 mt-14'>
           <div>
             <h1 className='text-4xl font-semibold text-gray-800 dark:text-gray-100 my-8'>Your {'{'} Assets {'}'}</h1>
+          </div>
+          <div>
+            {/* Modal overlay */}
+            {isModalOpen &&
+              <div className='fixed inset-0 bg-black bg-opacity-70 z-40' />}
+            {/* Main modal */}
+            <div id='defaultModal' className={`fixed inset-0 z-50 flex items-center justify-center w-full h-full ${isModalOpen ? '' : 'hidden'}`}>
+              <div className='relative z-40 p-4 w-full max-w-2xl md:h-auto bg-white rounded-lg shadow dark:bg-slate-800 sm:p-5 mx-8'>
+                {/* Modal content */}
+                <div className='relative p-4 rounded-lg dark:bg-slate-800 sm:p-5'>
+                  {/* Modal header */}
+                  <div className='flex justify-between items-center pb-4 mb-4 rounded-t border-b sm:mb-5 dark:border-slate-600'>
+                    <h3 className='text-lg font-semibold text-slate-900 dark:text-white'>
+                      Add Investment
+                    </h3>
+                    <button type='button' className='text-slate-400 bg-transparent hover:bg-slate-200 hover:text-slate-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-slate-600 dark:hover:text-white' onClick={() => setIsModalOpen(false)}>
+                      <svg aria-hidden='true' className='w-5 h-5' fill='currentColor' viewBox='0 0 20 20' xmlns='http://www.w3.org/2000/svg'>
+                        <path fillRule='evenodd' d='M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z' clipRule='evenodd' />
+                      </svg>
+                      <span className='sr-only'>Close modal</span>
+                    </button>
+                  </div>
+                  {/* Modal body */}
+                  <form action='#'>
+                    <div className='grid gap-4 mb-4 sm:grid-cols-2'>
+                      <div>
+                        <label htmlFor='name' className='block mb-2 text-sm font-medium text-slate-900 dark:text-white'>Name</label>
+                        <input type='text' name='name' id='name' className='bg-slate-50 border border-slate-300 text-slate-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-slate-700 dark:border-slate-600 dark:placeholder-slate-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500' placeholder='Investment Name (E.g. Microsoft)' required='' />
+                      </div>
+                      <div>
+                        <label htmlFor='identification' className='block mb-2 text-sm font-medium text-slate-900 dark:text-white'>Identification</label>
+                        <input type='text' name='identification' id='identification' className='bg-slate-50 border border-slate-300 text-slate-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-slate-700 dark:border-slate-600 dark:placeholder-slate-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500' placeholder='Identification (E.g. Stock Ticker)' required='' />
+                      </div>
+                      <div>
+                        <label htmlFor='units' className='block mb-2 text-sm font-medium text-slate-900 dark:text-white'>Units</label>
+                        <input type='number' name='units' id='units' className='bg-slate-50 border border-slate-300 text-slate-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-slate-700 dark:border-slate-600 dark:placeholder-slate-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500' placeholder='45' required='' />
+                      </div>
+                      <div>
+                        <label htmlFor='category' className='block mb-2 text-sm font-medium text-slate-900 dark:text-white'>Category</label>
+                        <select id='category' className='bg-slate-50 border border-slate-300 text-slate-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-slate-700 dark:border-slate-600 dark:placeholder-slate-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500'>
+                          <option selected=''>Select category</option>
+                          <option value='Stocks' id='Stocks'>Stocks</option>
+                          <option value='Bonds' id='Bonds'>Bonds</option>
+                          <option value='Funds' id='Funds'>Funds</option>
+                          <option value='Commodities' id='Commodities'>Commodities</option>
+                          <option value='Crypto' id='Crypto'>Crypto</option>
+                          <option value='Property' id='Property'>Property</option>
+                          <option value='Vehicles' id='Vehicles'>Vehicles</option>
+                          <option value='Jewelery' id='Jewelery'>Vehicles</option>
+                          <option value='Miscellaneous' id='Miscellaneous'>Miscellaneous</option>
+                        </select>
+                      </div>
+                      <div className='sm:col-span-2'>
+                        <label htmlFor='description' className='block mb-2 text-sm font-medium text-slate-900 dark:text-white'>Description</label>
+                        <textarea id='description' rows='4' className='block p-2.5 w-full text-sm text-slate-900 bg-slate-50 rounded-lg border border-slate-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-slate-700 dark:border-slate-600 dark:placeholder-slate-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500' placeholder='Write Investment description here' />
+                      </div>
+                    </div>
+                    <button type='submit' className='text-white inline-flex items-center bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800'>
+                      <svg className='mr-1 -ml-1 w-6 h-6' fill='currentColor' viewBox='0 0 20 20' xmlns='http://www.w3.org/2000/svg'><path fillRule='evenodd' d='M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z' clipRule='evenodd' /></svg>
+                      Add new Investment
+                    </button>
+                  </form>
+                </div>
+              </div>
+            </div>
           </div>
           <div className='relative overflow-x-auto rounded-xl'>
             <div className='pb-4 flex justify-between items-center'>
@@ -15,7 +85,7 @@ const AssetList = () => {
                 </div>
                 <input type='text' id='table-search' className='block p-2 pl-10 ml-0.5 text-sm text-slate-900 border border-slate-300 rounded-lg w-60 lg:w-80 bg-slate-50 focus:ring-slate-600 focus:border-slate-700 dark:bg-slate-700 dark:border-slate-400 dark:placeholder-slate-400 dark:text-white dark:focus:ring-slate-500 dark:focus:border-slate-200' placeholder='Search...' />
               </div>
-              <button className='ml-2 mt-1 py-2 px-3 bg-blue-600 hover:bg-blue text-white font-semibold rounded-lg shadow-md'>
+              <button id='defaultModalButton' onClick={() => setIsModalOpen(true)} className='ml-2 mt-1 py-2 px-3 bg-blue-600 hover:bg-blue-700 hover:bg-blue text-white font-semibold rounded-lg shadow-md'>
                 <svg className='w-5 h-5' viewBox='0 0 20 20' fill='currentColor'><path fillRule='evenodd' clipRule='evenodd' d='M11 9V5a1 1 0 00-2 0v4H5a1 1 0 100 2h4v4a1 1 0 102 0v-4h4a1 1 0 100-2h-4z' /></svg>
               </button>
             </div>
@@ -54,13 +124,13 @@ const AssetList = () => {
                     </div>
                   </td>
                   <th scope='row' className='px-6 py-4 font-medium text-slate-900 whitespace-nowrap dark:text-white'>
-                    S&P 500 - Vanguard
+                    Raytheon Technologies Corp
                   </th>
                   <td className='px-6 py-4 hidden lg:table-cell'>
-                    $VOO
+                    $RTX
                   </td>
                   <td className='px-6 py-4 hidden lg:table-cell'>
-                    Funds
+                    Stocks
                   </td>
                   <td className='px-6 py-4 hidden lg:table-cell'>
                     78
