751aea45a3ee36de8cc759721b672e93f34e310c
max whitehead <fifes-catkin.0r@icloud.com>
2023-05-20 19:34:19 +0100

    Changes 2

--- app/src/components/HoverMap.jsx
+++ app/src/components/HoverMap.jsx
@@ -17,8 +17,8 @@ const mapStyles = {
 const geographyStyle = {
   default: {
     fill: '#D6D6DA',
-    stroke: '#94a3b8', // Color of the outline
-    strokeWidth: 0.25 // Width of the outline
+    stroke: '#94a3b8', // Outline Color
+    strokeWidth: 0.25 // Outline Width
   },
   hover: {
     fill: '#3B82F6',
