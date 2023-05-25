20e202b78327aed664e063235e0538dcaebb9c2f
Santiago Ospina Idrobo <santiagospidrobo15@gmail.com>
2023-05-25 09:21:51 +0200

    create authSlice with credentials and token

--- app/src/services/store.js
+++ app/src/services/store.js
@@ -1,6 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { apiSlice } from './api/apiSlice'
 import { setupListeners } from '@reduxjs/toolkit/dist/query'
+import { authSlice } from '../features/authentication/hooks/authSlice'
 
 export const store = configureStore({
   reducer: {
