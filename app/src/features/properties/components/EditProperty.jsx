1e14d9140c379f939ce3758890da83ea0711c60b
Santiago Ospina Idrobo <santiagospidrobo15@gmail.com>
2023-06-16 15:29:03 -0500

    display date in the update screen fixed

--- app/src/features/properties/components/EditProperty.jsx
+++ app/src/features/properties/components/EditProperty.jsx
@@ -33,10 +33,20 @@ const EditProperty = () => {
 
   const property = properties.find(property => property.id === id)
 
+  const formatDate = (date) => {
+    const year = date.getFullYear()
+    const month = String(date.getMonth() + 1).padStart(2, '0')
+    const day = String(date.getDate()).padStart(2, '0')
+    return `${year}-${month}-${day}`
+  }
+
   const [name, setName] = useState(property.name)
   const [country, setCountry] = useState(property.country)
   const [currency, setCurrency] = useState(property.currency)
-  const [date, setDate] = useState(property.date)
+  const [date, setDate] = useState(() => {
+    const originalDate = new Date(property.date)
+    return formatDate(originalDate)
+  })
   const [value, setValue] = useState(property.value)
   const [taxStatus, setTaxStatus] = useState(property.taxStatus)
   const [type, setType] = useState(property.type)
