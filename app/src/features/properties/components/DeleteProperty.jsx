8c0d60949d67a77290cca5e7e7a5016d9ed0f0f3
Santiago Ospina Idrobo <santiagospidrobo15@gmail.com>
2023-06-16 17:46:17 -0500

    component to delete property done

--- app/src/features/properties/components/DeleteProperty.jsx
+++ app/src/features/properties/components/DeleteProperty.jsx
@@ -1,1 +1,63 @@
-// const DeleteProperty = ({ propertyId }) => {
+import Properties from '../index'
+import { Link, useNavigate, useParams } from 'react-router-dom'
+import { useEffect } from 'react'
+
+import { useDeletePropertyMutation, useGetPropertiesQuery } from '../services/propertiesApiSlice'
+
+const DeleteProperty = () => {
+  const { id } = useParams()
+
+  const {
+    data: properties
+  } = useGetPropertiesQuery('propertiesList', {
+    refetchOnMountOrArgChange: true,
+    refetchOnFocus: false,
+    pollingInterval: 20000
+  })
+
+  const [deleteProperty, {
+    isSuccess,
+    isError,
+    error
+  }] = useDeletePropertyMutation()
+
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if (isSuccess) {
+      navigate('/investments/properties/')
+    }
+  }, [id, isSuccess, navigate])
+
+  const property = properties.find(property => property.id === id)
+
+  const handleConfirm = async (e) => {
+    e.preventDefault()
+
+    console.log('id to delete', id)
+
+    await deleteProperty({ id })
+  }
+
+  const errClass = isError ? 'errorMsg text-red-500' : 'offscreen'
+
+  const content = (
+    <Properties backTo={`/investments/properties/${id}`}>
+      <p className={errClass}>{error?.data?.message}</p>
+      {
+        (error?.data?.error === 'Forbidden token')
+          ? (
+            <button className='my-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded' onClick={() => navigate('/login')}>Login Again</button>
+            )
+          : null
+      }
+      <h1>Do you want to delete {property.name}?</h1>
+      <button className='my-5 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded' onClick={handleConfirm}>Yes</button>
+      <Link to={`../${id}`} className='my-5 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>No</Link>
+    </Properties>
+  )
+
+  return content
+}
+
+export default DeleteProperty
--- app/src/features/properties/components/Property.jsx
+++ app/src/features/properties/components/Property.jsx
@@ -1,6 +1,6 @@
 import Properties from '../index.jsx'
 import { useNavigate, useParams } from 'react-router-dom'
-import { useGetPropertiesQuery, useDeletePropertyMutation } from '../services/propertiesApiSlice'
+import { useGetPropertiesQuery } from '../services/propertiesApiSlice'
 import MapChart from '../../../components/HoverMap.jsx'
 import { useState } from 'react'
 
@@ -14,8 +14,6 @@ const Property = () => {
 
   let content
 
-  const [confirmWindow, setConfirmWindow] = useState(false)
-
   const {
     data: properties,
     isLoading,
@@ -27,18 +25,6 @@ const Property = () => {
     refetchOnFocus: false,
     pollingInterval: 20000
   })
-
-  const [deleteProperty, {
-    isSuccess: isDeleteSuccess
-  }] = useDeletePropertyMutation()
-
-  const handleConfirm = () => {
-    deleteProperty({ id })
-  }
-
-  if (isDeleteSuccess) {
-    navigate('/investments/properties')
-  }
 
   if (isLoading) {
     return (
@@ -84,135 +70,119 @@ const Property = () => {
 
     if (property) {
       const handleEdit = () => navigate('./edit')
+      const handleDelete = () => navigate('./delete')
 
-      const handleDelete = () => setConfirmWindow(true)
-      const handleBack = () => setConfirmWindow(false)
-
-      console.log('confirm window: ', confirmWindow)
-
-      if (confirmWindow) {
-        content = (
-          <Properties title={property.name}>
-            <h1>Do you want to delete this property?</h1>
-            <button className='my-5 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded' onClick={handleConfirm}>Yes</button>
-            <button className='my-5 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded' onClick={handleBack}>No</button>
-          </Properties>
-        )
-      }
-
-      if (!confirmWindow) {
-        content = (
-          <Properties backTo='/investments/properties'>
-            <div className='flex justify-between items-center mb-5 md:mb-10'>
-              <h1 className='text-3xl md:text-4xl font-bold text-slate-800 dark:text-white'>{property.name}</h1>
-              <div className='flex flex-row gap-2 md:gap-4 bg-slate-300 dark:bg-slate-800 p-2 rounded-full'>
-                <button className=' bg-blue-500 hover:bg-blue-700 text-white font-bold p-3 rounded-full' onClick={handleEdit}>
-                  <svg className='w-4 h-4 md:w-6 md:h-6' fill='none' stroke='currentColor' strokeWidth='1.5' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg' aria-hidden='true'>
-                    <path strokeLinecap='round' strokeLinejoin='round' d='M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10' />
-                  </svg>
-                </button>
-                <button className=' bg-blue-500 hover:bg-blue-700 text-white font-bold p-3 rounded-full' onClick={handleDelete}>
-                  <svg className='w-4 h-4 md:w-6 md:h-6' fill='none' stroke='currentColor' strokeWidth={1.5} viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg' aria-hidden='true'>
-                    <path strokeLinecap='round' strokeLinejoin='round' d='M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0' />
-                  </svg>
-                </button>
+      content = (
+        <Properties backTo='/investments/properties'>
+          <div className='flex justify-between items-center mb-5 md:mb-10'>
+            <h1 className='text-3xl md:text-4xl font-bold text-slate-800 dark:text-white'>{property.name}</h1>
+            <div className='flex flex-row gap-2 md:gap-4 bg-slate-300 dark:bg-slate-800 p-2 rounded-full'>
+              <button className=' bg-blue-500 hover:bg-blue-700 text-white font-bold p-3 rounded-full' onClick={handleEdit}>
+                <svg className='w-4 h-4 md:w-6 md:h-6' fill='none' stroke='currentColor' strokeWidth='1.5' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg' aria-hidden='true'>
+                  <path strokeLinecap='round' strokeLinejoin='round' d='M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10' />
+                </svg>
+              </button>
+              <button className=' bg-blue-500 hover:bg-blue-700 text-white font-bold p-3 rounded-full' onClick={handleDelete}>
+                <svg className='w-4 h-4 md:w-6 md:h-6' fill='none' stroke='currentColor' strokeWidth={1.5} viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg' aria-hidden='true'>
+                  <path strokeLinecap='round' strokeLinejoin='round' d='M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0' />
+                </svg>
+              </button>
+            </div>
+          </div>
+          <div className='grid grid-cols-1 lg:grid-cols-2 gap-4 mb-4'>
+            <div className='relative bg-slate-50 dark:bg-slate-800 p-4 rounded-xl aspect-w-1 aspect-h-1'>
+              {/* <img src={property.image} alt='' className='mb-3 w-full rounded-xl object-cover' style={{ aspectRatio: '1/1' }} /> */}
+              <div className='mb-3 h-full w-full rounded-lg overflow-hidden'>
+                <img
+                  src={property.photo}
+                  className='object-cover w-full h-full transform transition-all duration-500 hover:scale-110'
+                />
               </div>
             </div>
-            <div className='grid grid-cols-1 lg:grid-cols-2 gap-4 mb-4'>
-              <div className='relative bg-slate-50 dark:bg-slate-800 p-4 rounded-xl aspect-w-1 aspect-h-1'>
-                {/* <img src={property.image} alt='' className='mb-3 w-full rounded-xl object-cover' style={{ aspectRatio: '1/1' }} /> */}
-                <div className='mb-3 h-full w-full rounded-lg overflow-hidden'>
-                  <img
-                    src={property.photo}
-                    className='object-cover w-full h-full transform transition-all duration-500 hover:scale-110'
-                  />
-                </div>
-              </div>
-              <div className='rounded-xl bg-slate-50 aspect-w-1 aspect-h-1 dark:bg-slate-800 shadow-lg flex justify-center items-center p-2 lg:p-4'>
-                <div className='min-w-full'>
-                  <div className='h-full p-4'>
-                    <div className='grid grid-cols-2 grid-rows-2 gap-4 xl:gap-8 text-slate-700 dark:text-white'>
-                      <div className='bg-slate-200 dark:bg-slate-600 p-4 rounded-xl shadow-lg overflow-hidden flex flex-col justify-center'>
-                        <h2 className='text-lg xl:text-2xl font-semibold'>Purchased</h2>
-                        <h3 className='text-md xl:text-xl text-slate-600 dark:text-slate-300'>{new Date(property.date).toLocaleDateString()}</h3>
-                      </div>
-                      <div className='bg-slate-200 dark:bg-slate-600 p-4 rounded-xl shadow-lg overflow-hidden flex flex-col justify-center'>
-                        <h2 className='text-lg xl:text-2xl font-semibold'>Value</h2>
-                        <h3 className='text-md xl:text-xl text-slate-600 dark:text-slate-300'>{property.currency} {property.value}</h3>
-                      </div>
-                      <div className='bg-slate-200 dark:bg-slate-600 p-4 rounded-xl shadow-lg overflow-hidden flex flex-col justify-center'>
-                        <h2 className='text-lg xl:text-2xl font-semibold'>Type</h2>
-                        <h3 className='text-md xl:text-xl text-slate-600 dark:text-slate-300'>{property.type}</h3>
-                      </div>
-                      <div className='bg-slate-200 dark:bg-slate-600 p-4 rounded-xl shadow-lg overflow-hidden flex flex-col justify-center'>
-                        <h2 className='text-lg xl:text-2xl font-semibold'>Tax Status</h2>
-                        <h3 className='text-md xl:text-xl text-slate-600 dark:text-slate-300'>{property.taxStatus}</h3>
-                      </div>
+            <div className='rounded-xl bg-slate-50 aspect-w-1 aspect-h-1 dark:bg-slate-800 shadow-lg flex justify-center items-center p-2 lg:p-4'>
+              <div className='min-w-full'>
+                <div className='h-full p-4'>
+                  <div className='grid grid-cols-2 grid-rows-2 gap-4 xl:gap-8 text-slate-700 dark:text-white'>
+                    <div className='bg-slate-200 dark:bg-slate-600 p-4 rounded-xl shadow-lg overflow-hidden flex flex-col justify-center'>
+                      <h2 className='text-lg xl:text-2xl font-semibold'>Purchased</h2>
+                      <h3 className='text-md xl:text-xl text-slate-600 dark:text-slate-300'>{new Date(property.date).toLocaleDateString()}</h3>
                     </div>
-                    <div className='py-2 px-4 mt-4 xl:mt-8 bg-slate-200 dark:bg-slate-600 text-slate-700 dark:text-white rounded-xl shadow-lg'>
-                      <h1 className='py-2 text-md md:text-lg font-semibold'>Files</h1>
-                      <div className='relative overflow-x-auto shadow-md sm:rounded-lg'>
-                        <table className='w-full text-sm text-left text-slate-500 dark:text-slate-400'>
-                          <thead className='text-xs text-slate-700 uppercase bg-slate-50 dark:bg-slate-700 dark:text-slate-400'>
-                            <tr>
-                              <th scope='col' className='px-6 py-4'>
-                                Name
+                    <div className='bg-slate-200 dark:bg-slate-600 p-4 rounded-xl shadow-lg overflow-hidden flex flex-col justify-center'>
+                      <h2 className='text-lg xl:text-2xl font-semibold'>Value</h2>
+                      <h3 className='text-md xl:text-xl text-slate-600 dark:text-slate-300'>{property.currency} {property.value}</h3>
+                    </div>
+                    <div className='bg-slate-200 dark:bg-slate-600 p-4 rounded-xl shadow-lg overflow-hidden flex flex-col justify-center'>
+                      <h2 className='text-lg xl:text-2xl font-semibold'>Type</h2>
+                      <h3 className='text-md xl:text-xl text-slate-600 dark:text-slate-300'>{property.type}</h3>
+                    </div>
+                    <div className='bg-slate-200 dark:bg-slate-600 p-4 rounded-xl shadow-lg overflow-hidden flex flex-col justify-center'>
+                      <h2 className='text-lg xl:text-2xl font-semibold'>Tax Status</h2>
+                      <h3 className='text-md xl:text-xl text-slate-600 dark:text-slate-300'>{property.taxStatus}</h3>
+                    </div>
+                  </div>
+                  <div className='py-2 px-4 mt-4 xl:mt-8 bg-slate-200 dark:bg-slate-600 text-slate-700 dark:text-white rounded-xl shadow-lg'>
+                    <h1 className='py-2 text-md md:text-lg font-semibold'>Files</h1>
+                    <div className='relative overflow-x-auto shadow-md sm:rounded-lg'>
+                      <table className='w-full text-sm text-left text-slate-500 dark:text-slate-400'>
+                        <thead className='text-xs text-slate-700 uppercase bg-slate-50 dark:bg-slate-700 dark:text-slate-400'>
+                          <tr>
+                            <th scope='col' className='px-6 py-4'>
+                              Name
+                            </th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          {files.slice(currentPage * itemsPerPage, (currentPage + 1) * itemsPerPage).map((file, index) => (
+                            <tr key={index} className={(index + currentPage * itemsPerPage) % 2 === 0 ? 'bg-white border-b dark:bg-slate-800 dark:border-slate-700' : 'border-b bg-slate-50 dark:bg-slate-800 dark:border-slate-700'}>
+                              <th scope='row' className='px-6 py-4 font-medium text-slate-900 whitespace-nowrap dark:text-white'>
+                                {file}
                               </th>
                             </tr>
-                          </thead>
-                          <tbody>
-                            {files.slice(currentPage * itemsPerPage, (currentPage + 1) * itemsPerPage).map((file, index) => (
-                              <tr key={index} className={(index + currentPage * itemsPerPage) % 2 === 0 ? 'bg-white border-b dark:bg-slate-800 dark:border-slate-700' : 'border-b bg-slate-50 dark:bg-slate-800 dark:border-slate-700'}>
-                                <th scope='row' className='px-6 py-4 font-medium text-slate-900 whitespace-nowrap dark:text-white'>
-                                  {file}
-                                </th>
-                              </tr>
-                            ))}
-                          </tbody>
-                        </table>
-                      </div>
-                      <div className='p-3'>
-                        {currentPage > 0 && <button onClick={handlePrevious} className='mr-4'>❮ Previous</button>}
-                        {(currentPage + 1) * itemsPerPage < files.length && <button onClick={handleNext}>Next ❯</button>}
-                      </div>
+                          ))}
+                        </tbody>
+                      </table>
+                    </div>
+                    <div className='p-3'>
+                      {currentPage > 0 && <button onClick={handlePrevious} className='mr-4'>❮ Previous</button>}
+                      {(currentPage + 1) * itemsPerPage < files.length && <button onClick={handleNext}>Next ❯</button>}
                     </div>
                   </div>
                 </div>
               </div>
             </div>
-            <div className='grid grid-cols-1 lg:grid-cols-2 gap-4 mb-4'>
-              <div className='rounded-xl bg-slate-50 aspect-w-1 aspect-h-2 col-span-1 lg:col-span-1 lg:row-span-2 dark:bg-slate-800 shadow-lg'>
-                <div className='items-center'>
-                  <p className='text-xl lg:text-2xl px-4 pt-4 text-slate-400 dark:text-slate-500'>Property Location</p>
-                </div>
-                <div className='p-5 rounded-lg'>
-                  <div className='bg-slate-100 dark:bg-slate-700 rounded-lg shadow-xl'>
-                    <MapChart />
-                  </div>
-                </div>
+          </div>
+          <div className='grid grid-cols-1 lg:grid-cols-2 gap-4 mb-4'>
+            <div className='rounded-xl bg-slate-50 aspect-w-1 aspect-h-2 col-span-1 lg:col-span-1 lg:row-span-2 dark:bg-slate-800 shadow-lg'>
+              <div className='items-center'>
+                <p className='text-xl lg:text-2xl px-4 pt-4 text-slate-400 dark:text-slate-500'>Property Location</p>
               </div>
-              <div className='rounded-xl bg-slate-50 aspect-w-1 aspect-h-1 dark:bg-slate-800 shadow-lg p-4'>
-                <div className='p-4 bg-slate-200 dark:bg-slate-600 text-slate-700 dark:text-white rounded-xl shadow-lg h-full flex flex-col justify-center'>
-                  <p className='text-xl lg:text-2xl px-4 mb-2'>Address</p>
-                  <p className='text-md lg:text-lg px-4'>{property.address}</p>
-                  <p className='text-md lg:text-lg px-4'>{property.city}</p>
-                  <p className='text-md lg:text-lg px-4'>{property.zip}</p>
-                  <p className='text-md lg:text-lg px-4'>{property.country}</p>
-                </div>
-              </div>
-              <div className='rounded-xl bg-slate-50 aspect-w-1 aspect-h-1 dark:bg-slate-800 shadow-lg p-4'>
-                <div className='p-4 bg-slate-200 dark:bg-slate-600 text-slate-700 dark:text-white rounded-xl shadow-lg h-full flex flex-col justify-center'>
-                  <p className='text-xl lg:text-2xl px-4 mb-2'>Contact</p>
-                  <p className='text-md lg:text-lg px-4'>Account Number</p>
-                  <p className='text-md lg:text-lg px-4'>Email</p>
-                  <p className='text-md lg:text-lg px-4'>Phone</p>
-                  <p className='text-md lg:text-lg px-4'>Company Address</p>
+              <div className='p-5 rounded-lg'>
+                <div className='bg-slate-100 dark:bg-slate-700 rounded-lg shadow-xl'>
+                  <MapChart />
                 </div>
               </div>
             </div>
-          </Properties>
-        )
-      }
+            <div className='rounded-xl bg-slate-50 aspect-w-1 aspect-h-1 dark:bg-slate-800 shadow-lg p-4'>
+              <div className='p-4 bg-slate-200 dark:bg-slate-600 text-slate-700 dark:text-white rounded-xl shadow-lg h-full flex flex-col justify-center'>
+                <p className='text-xl lg:text-2xl px-4 mb-2'>Address</p>
+                <p className='text-md lg:text-lg px-4'>{property.address}</p>
+                <p className='text-md lg:text-lg px-4'>{property.city}</p>
+                <p className='text-md lg:text-lg px-4'>{property.zip}</p>
+                <p className='text-md lg:text-lg px-4'>{property.country}</p>
+              </div>
+            </div>
+            <div className='rounded-xl bg-slate-50 aspect-w-1 aspect-h-1 dark:bg-slate-800 shadow-lg p-4'>
+              <div className='p-4 bg-slate-200 dark:bg-slate-600 text-slate-700 dark:text-white rounded-xl shadow-lg h-full flex flex-col justify-center'>
+                <p className='text-xl lg:text-2xl px-4 mb-2'>Contact</p>
+                <p className='text-md lg:text-lg px-4'>Account Number</p>
+                <p className='text-md lg:text-lg px-4'>Email</p>
+                <p className='text-md lg:text-lg px-4'>Phone</p>
+                <p className='text-md lg:text-lg px-4'>Company Address</p>
+              </div>
+            </div>
+          </div>
+        </Properties>
+      )
     }
   }
 
