5e400e06e2b16f706d07845cad5cea326f56b95b
max whitehead <fifes-catkin.0r@icloud.com>
2023-06-20 18:01:54 +0100

    Modified Add Property Button

--- app/src/components/ModalAddButton.jsx
+++ app/src/components/ModalAddButton.jsx
@@ -1,11 +1,13 @@
 const ModalAddButton = ({ onClick, children }) => {
   return (
-    <button id='defaultModalButton' className='py-2 px-3 bg-blue-600 hover:bg-blue-700 text-white font-semibold rounded-lg shadow-md' onClick={onClick}>
-      <svg className='w-5 h-5' viewBox='0 0 20 20' fill='currentColor'>
-        <path fillRule='evenodd' clipRule='evenodd' d='M11 9V5a1 1 0 00-2 0v4H5a1 1 0 100 2h4v4a1 1 0 102 0v-4h4a1 1 0 100-2h-4z' />
-      </svg>
-      {children}
-    </button>
+    <div className='bg-slate-300 dark:bg-slate-400 hover:bg-blue-400 hover:dark:bg-blue-100 transition-all ease-in-out duration-150 rounded-full p-1'>
+      <button id='defaultModalButton' className='p-2 bg-blue-600 hover:bg-blue-700 active:bg-blue-800 text-white font-semibold rounded-full shadow-md' onClick={onClick}>
+        <svg className='w-5 h-5' viewBox='0 0 20 20' fill='currentColor'>
+          <path fillRule='evenodd' clipRule='evenodd' d='M11 9V5a1 1 0 00-2 0v4H5a1 1 0 100 2h4v4a1 1 0 102 0v-4h4a1 1 0 100-2h-4z' />
+        </svg>
+        {children}
+      </button>
+    </div>
   )
 }
 
