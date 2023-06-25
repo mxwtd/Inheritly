0b350e737356621ea4becc8a8e7b3689827cd464
max whitehead <fifes-catkin.0r@icloud.com>
2023-06-25 16:18:27 +0100

    Modified Investment Card Grid, better gradient 3

--- app/src/components/InvestmentCard.jsx
+++ app/src/components/InvestmentCard.jsx
@@ -2,11 +2,11 @@ import { Link } from 'react-router-dom'
 
 const InvestmentCard = ({ cardName, path, balance, currency }) => {
   return (
-    <Link to={`/investments/${path}`} className='rounded-xl bg-slate-400/20 backdrop-blur-md border border-slate-300 dark:border-slate-700 h-48 md:hover:scale-102 transition-all ease-in-out duration-200 cursor-pointer shadow-md hover:shadow-xl hover:brightness-105'>
+    <Link to={`/investments/${path}`} className='rounded-xl bg-slate-400/20 backdrop-blur-md border border-slate-300 dark:border-slate-700 h-48 md:hover:scale-102 transition-all ease-in-out duration-200 cursor-pointer shadow-lg hover:shadow-2xl hover:brightness-105'>
       <div className='flex flex-col justify-between h-full p-6 back'>
         <p className='text-lg lg:text-3xl text-slate-700 dark:text-slate-200 font-semibold'>{cardName}</p>
-        <div className='flex justify-start'>
-          <div className='flex flex-col text-left rounded-2xl'>
+        <div className='flex justify-end'>
+          <div className='flex flex-col text-right rounded-2xl'>
             <p className='text-md lg:text-2xl text-slate-600 dark:text-slate-200 font-bold'>Balance</p>
             <p className='text-sm lg:text-lg text-slate-500 dark:text-slate-200'>$15,000</p>
           </div>
