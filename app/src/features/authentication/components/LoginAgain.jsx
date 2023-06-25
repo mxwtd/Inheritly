1ed46953eeda5c6e57fe734852c21f1b41ed0d21
max whitehead <fifes-catkin.0r@icloud.com>
2023-06-25 23:19:42 +0100

    Added verification error state (red Borders) to AddProperty form

--- app/src/features/authentication/components/LoginAgain.jsx
+++ app/src/features/authentication/components/LoginAgain.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom'
 
 const LoginAgain = ({ errorMsg }) => {
   return (
-    <div className='flex flex-col items-center justify-center h-screen'>
+    <div style={{ height: '100vh', overflowY: 'hidden' }} className='flex flex-col items-center justify-center'>
       <div className='bg-white dark:bg-slate-700 p-8 rounded-2xl flex flex-col items-center justify-center gap-6 shadow-xl'>
         <img className='w-10 h-10 md:w-12 md:h-12 mr-2' src='https://res.cloudinary.com/djr22sgp3/image/upload/v1684267350/Inheritly_-_Third_design_qodghx.png' />
         <span className='self-center text-lg md:text-2xl font-semibold whitespace-nowrap dark:text-white'>Inheritly</span>
@@ -14,7 +14,6 @@ const LoginAgain = ({ errorMsg }) => {
         </Link>
       </div>
     </div>
-
   )
 }
 
