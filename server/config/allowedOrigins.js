a91a3f1af57736447f9c6c47089594c49d182c10
Santiago Ospina Idrobo <santiagospidrobo15@gmail.com>
2023-07-06 16:48:00 -0500

    preparing for deployment

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -3,7 +3,7 @@ const allowedOrigins = [
   'http://localhost:3001',
   'http://127.0.0.1:5173',
   'http://localhost:5173',
-  'https://inheritly-app-d2c2366c08d3.herokuapp.com/'
+  'https://inheritly-app-d2c2366c08d3.herokuapp.com'
 ]
 
 module.exports = allowedOrigins
