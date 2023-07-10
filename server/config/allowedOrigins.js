7db2c99f05d9284844539ca74042d7f7d4232a4e
max whitehead <fifes-catkin.0r@icloud.com>
2023-07-10 23:03:19 +0100

    Added dashboard link 3

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,8 +1,7 @@
 const allowedOrigins = [
   'http://127.0.0.1:5173',
   'http://localhost:5173',
-  // 'https://inheritly-app-d2c2366c08d3.herokuapp.com',
-  'https://lustrous-belekoy-8fb80a.netlify.app'
+  'https://dashboard.inheritly.app'
 ]
 
 module.exports = allowedOrigins
